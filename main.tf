a7e759956b0d6babb426c8664eeb0f4d8512aa19
juedra11 <90887198+juedra11@users.noreply.github.com>
2024-12-03 19:03:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource "docker_container" "web_nextcloud"{
 
     volumes {
         ## host_path = "/mnt/prueba_qnap"
-        host_path = "./volumen"
+        host_path = "../../volumen"
         container_path = "/var/html/www/data"
     }
 
